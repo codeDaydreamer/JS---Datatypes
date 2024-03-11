911ea2cc41af4ec906125e7d38d0134774b67283
Code Daydreamer <codedaydreamer@gmail.com>
2024-03-11 13:29:20 +0300

    Day 1

---
+++ index.js
@@ -0,0 +1,16 @@
+//editing html elements using .getElementById
+
+document.getElementById('h1').textContent ='Hello there';
+document.getElementById('p1').textContent ='This is my irst ever paragraph';
+
+//variables in js
+let age = 20;
+let price = 55.66;
+let avgpoints = 6.8;
+let username = 'Daydreamer';
+
+console.log(`Hello there ${username}, you are ${age} years old and your GPA is ${avgpoints}`);
+
+//creating window alerts
+
+window.alert(`Hello there!!`);
